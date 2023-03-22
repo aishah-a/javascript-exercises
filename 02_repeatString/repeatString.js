99a8c0fb68ae07f10ca85c23c601255b8f0a6169
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-03-21 20:17:39 -0600

    "Create variable. Add loop."

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function('hey', 3) {
+    for (let i = 0; i <= 3; i++) {
+        return 'hey';
+    }
 };
+
+let repeatedString = repeatString;
+
 
 // Do not edit below this line
 module.exports = repeatString;

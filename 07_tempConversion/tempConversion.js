0ae0cf79c38a489ba83478c310bed57bd6fb27de
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-03-23 15:22:40 -0600

    Pass first two tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+// take temp in fahrenheit
+let tempCelsius = ((temp - 32) * (5/9));
+// apply formula to temp
+// round to one decimal place
+tempCelsius = tempCelsius.toFixed(1);
+tempCelsius = parseFloat(tempCelsius);
+// store temp in var
+return tempCelsius;
 };
 
 const convertToFahrenheit = function() {

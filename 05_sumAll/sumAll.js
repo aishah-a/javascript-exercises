c692e5003855d41b6b11f31e84f3d8feb9e92931
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-03-23 11:01:58 -0600

    Pass first test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    for (let i = num1; i < num2 + 1; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
+
+
+// number 1
+// number 2
+// return all numbers between = num = num 1 OR num > num1 AND num < num2
 
 // Do not edit below this line
 module.exports = sumAll;

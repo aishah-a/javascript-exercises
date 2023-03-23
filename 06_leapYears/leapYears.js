e030afb8391f65d705eaa5bcbe2d0787ffbe0db1
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-03-23 12:42:46 -0600

    Pass 4 out of 6 tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,14 @@
 const leapYears = function(year) {
     let isLeapYear = false;
+    // divisible by 4
     if (year % 4 === 0) {
+        isLeapYear = true;
+    // divisible by 100 AND 400 {
+    } else if ((year % 100 === 0) && (year % 400 === 0)) {
         isLeapYear = true;
     }
     return isLeapYear;
-};
+    
 
 // Do not edit below this line
 module.exports = leapYears;

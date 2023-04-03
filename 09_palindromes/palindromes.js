d20c309ccca01ebdbca13266e40eb439ab93f09d
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-03 15:13:28 -0600

    Create palindrome check function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let regex = /[\W_]/g; //remove all non alphanum chars
+    let string = str.toLowerCase().replace(regex, '');
+    let reverseString = string.split('').reverse().join('');
+    
+    return reverseString === string;
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;

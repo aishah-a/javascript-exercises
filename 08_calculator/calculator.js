5b91978627d203db985a7be7383fbe8b5d23400a
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-03 14:35:26 -0600

    Add factorialize, sum and power functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(num1, num2) {
 	return (num1 - num2);
 };
 
-const sum = function() {
-};
+const sum = function(array) {
+  let result = array.reduce((accumulator, currentVal) => {
+    return accumulator + currentVal;
+  }, 0);
+  return result;
+}
 
 const multiply = function(array) {
   let result = array.reduce((accumulator, currentVal) => {
@@ -17,13 +21,17 @@ const multiply = function(array) {
 }
 
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+    let result = 1
+    for (let i = 1; i <= num; i++) {
+      result = result * i;
+    }
+    return result;
+  }
 
 // Do not edit below this line
 module.exports = {

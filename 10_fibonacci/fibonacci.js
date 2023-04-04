a98160d74ed7c5cacf989250f7a181dc0a9d736f
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-04 15:03:01 -0600

    Create fibonacci function with for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+/*
+> A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+*/
 
-};
+function fibonacci(n) {
+    const fib = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    let finalFib = fib[fib.length -1];
+    return finalFib;
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;

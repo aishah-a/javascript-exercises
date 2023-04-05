51b0965dcbff43ac273901196dd3f102bfacbbbc
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-04 18:04:39 -0600

    Create getTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let newArray = array.map(a => a.title);
+    return newArray;
+}
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
